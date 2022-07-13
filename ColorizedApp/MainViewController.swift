eaf1f408fb44094228cd408005348dde918c9df9
meetIgor <iigoor.s@gmail.com>
2022-07-13 23:27:43 +0300

    released color transfer from 1st to 2nd ViewController and backward

---
+++ ColorizedApp/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  ColorizedApp
+//
+//  Created by igor s on 13.07.2022.
+//
+
+import UIKit
+
+protocol SettingsViewControllerDelegate {
+    func setColor(_ color:UIColor)
+}
+
+
+class MainViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.color = view.backgroundColor
+        settingsVC.delegate = self
+    }
+
+}
+
+// MARK: - SettingsViewControllerDelegate
+extension MainViewController: SettingsViewControllerDelegate {
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+}
